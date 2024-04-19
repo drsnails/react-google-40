be168e30a3a78a91ea2eb7a6e7950f166d679693
Stav Yaar Bar <anistu@gmail.com>
2024-04-19 22:39:05 +0300

    changed package json

--- babel.config.js
+++ babel.config.js
@@ -4,16 +4,17 @@ module.exports = {
             "@babel/preset-env",
             {
                 targets: {
-                    chrome: "40",
+                    chrome: "40"
                 },
                 useBuiltIns: "entry",
                 corejs: 3,
-            },
+                debug: true
+            }
         ],
-        "@babel/preset-react",
+        "@babel/preset-react"
     ],
     plugins: [
         "@babel/plugin-transform-arrow-functions",
         "@babel/plugin-transform-react-jsx",
-    ],
+    ]
 };
